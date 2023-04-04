1b5041ae56fa5d078ccfc91d99efe5586f5153ad
EzequielLG <A01734172@tec.mx>
2023-04-03 22:09:00 -0600

    Update 03-04-2023

--- blurring.cpp
+++ blurring.cpp
@@ -6,28 +6,84 @@
 
 using namespace std;
 
+vector<vector<vector<unsigned char>>> add_pixels(vector<vector<vector<unsigned char>>> matrix, int mask, long alto, long ancho) {
+  unsigned char white_pixel = '5';
+  vector<unsigned char> channels (3, white_pixel);
+
+  for (int i = 0; i < alto; i++) {
+    for (int j = 0; j < (mask - 1) / 2; j++) {
+      matrix[i].insert(matrix[i].begin(), channels);
+      matrix[i].insert(matrix[i].end(), channels);
+    }
+  }
+
+  vector<vector <unsigned char>> row ((ancho + mask - 1), channels);
+
+  for (int i = 0; i < (mask - 1) / 2; i ++) {
+    matrix.insert(matrix.begin(), row);
+    matrix.insert(matrix.end(), row);
+  }
+
+  return matrix;
+}
+
+void blur_image(vector<vector<vector<unsigned char>>> matrix, int mask, long alto, long ancho, FILE *outputImage) {
+  long firstRow = (matrix.size() - alto) / 2;
+  long lastRow = firstRow + alto - 1;
+  long lastColumn = firstRow + ancho - 1;
+  int outerPixels = (mask - 1) / 2;
+
+  for (long i = firstRow; i <= lastRow; i++) {
+    for (long j = firstRow; j <= lastColumn; j++) {
+      long colorWeight = 0;
+      for (long k = i - outerPixels; k <= i + outerPixels; k++) {
+        for (long l = j - outerPixels; l <= j + outerPixels; l++) {
+          colorWeight += matrix[k][l][0];
+        }
+      }
+      colorWeight /= mask * mask;
+      fputc(colorWeight, outputImage);
+      fputc(colorWeight, outputImage);
+      fputc(colorWeight, outputImage);
+    }
+  }
+  fclose(outputImage);
+}
+
 int main(){
 
   // omp_set_num_threads(NUM_THREADS);
   FILE *image, *outputImage;
-  image = fopen("f3.bmp","rb"); // Imagen original a transformar
-  outputImage = fopen("f4_blur.bmp","wb"); // Imagen transformada
+  image = fopen("TestingImages/sample.bmp","rb"); // Imagen original a transformar
+  outputImage = fopen("f3_blur.bmp","wb"); // Imagen transformada
   
+  int mask = 11;
   long ancho, alto;
-  unsigned char* ptr;
   unsigned char xx[54];
   vector<vector<vector<unsigned char>>> matrix;
   
   for(int i=0; i < 54; i++){
     xx[i] = fgetc(image);
+
+    if(i == 19 || i == 23){
+      xx[i] += (mask - 1) / 256;
+    }
+    
     fputc(xx[i], outputImage); // Copia cabecera a nueva imagen
   }
 
   ancho = (long)xx[20]*65536 + (long)xx[19]*256 + (long)xx[18];
   alto = (long)xx[24]*65536 + (long)xx[23]*256 + (long)xx[22];
+  cout << "Ancho" << endl;
+  cout << "xx[20]: " << (long)xx[20] << " (long)xx[20]*65536: " << (long)xx[20]*65536 << endl;
+  cout << "xx[19]: " << (long)xx[19] << " (long)xx[19]*256: " << (long)xx[19]*256 << endl;
+  cout << "xx[18]: " << (long)xx[18] << " (long)xx[18]: " << (long)xx[18] << endl;
+  cout << "Alto" << endl;
+  cout << "xx[24]: " << (long)xx[24] << " (long)xx[24]*65536: " << (long)xx[24]*65536 << endl;
+  cout << "xx[23]: " << (long)xx[23] << " (long)xx[23]*256: " << (long)xx[23]*256 << endl;
+  cout << "xx[22]: " << (long)xx[22] << " (long)xx[22]: " << (long)xx[22] << endl;
   cout << "alto img " << alto << endl;
   cout << "ancho img " << ancho << endl;
-  ptr = (unsigned char*)malloc(alto*ancho*3*sizeof(unsigned char));
 
   long padding = ancho%4;
 
@@ -47,7 +103,9 @@ int main(){
       b = fgetc(image);
       g = fgetc(image);
       r = fgetc(image);
+      // cout << "B: " << b << " G: " << g << " R: " << r << " ";
       unsigned char pixel = ((0.21*r) + (0.72*g) + (0.07*b));
+      // cout << "Pixel: " << pixel << endl;
       channels.push_back(pixel); // B
       channels.push_back(pixel); // G
       channels.push_back(pixel); // R
@@ -64,17 +122,10 @@ int main(){
 
   }
 
-  // for(int i = 0; !feof(image); i++){
-  //   unsigned char b, g, r;
-  //   b = fgetc(image);
-  //   g = fgetc(image);
-  //   r = fgetc(image);
-  //   temporal.push_back((unsigned char)((0.21*r) + (0.72*g) + (0.07*b)));
-  // }
+  vector<vector<vector<unsigned char>>> matrixAddPixels = add_pixels(matrix, 11, alto, ancho);
+  // blur_image(matrixAddPixels, 11, alto, ancho, outputImage);
 
-  // matrix.push_back(temporal);
-
-  for(vector<vector<unsigned char>> v : matrix){
+  for(vector<vector<unsigned char>> v : matrixAddPixels){
     for(vector<unsigned char> c : v){
       fputc(c[0], outputImage);
       fputc(c[1], outputImage);
@@ -82,24 +133,7 @@ int main(){
     }
   }
 
-  // ----------- YA FUNCIONA ------------- //
-  // for(vector<unsigned char> v : matrix){
-  //   for(int i = 0; i < v.size(); i++){
-  //     fputc(v[i], outputImage);
-  //     fputc(v[i+1], outputImage);
-  //     fputc(v[i+2], outputImage);
-  //   }
-  // }
-  
-  // for(int i = 0; i < alto*ancho*3; ++i){
-  //   fputc(matrix[i], outputImage);
-  //   fputc(matrix[i+1], outputImage);
-  //   fputc(matrix[i+2], outputImage);
-  // }
-
-  free(ptr);
   fclose(image);
-  fclose(outputImage);
   return 0;
 
 }
